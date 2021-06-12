b2cd804d373a13b13bdc7058640d1dc1c4d24938
Nadim Radjab <nadimradjab94@gmail.com>
2021-06-12 16:21:20 +0200

    Project Task

--- src/domstuff.js
+++ src/domstuff.js
@@ -1,10 +1,12 @@
 import { formMaker } from './project'
 
-const btnProject = document.querySelector('#projectBtn')
+const btnProject = document.querySelector('#projectBtn');
+const popupProjectDiv = document.querySelector('.popupProject');
 
 
 function test() {
     btnProject.addEventListener('click', () => {
+        popupProjectDiv.style.display = 'flex'
         formMaker()
         btnProject.disabled = true;
     })
--- src/project.js
+++ src/project.js
@@ -3,43 +3,86 @@ import { Project, Todo } from './projectClass'
 
 const projectArr = [];
 
-const mainDiv = document.querySelector('.main')
+const mainDiv = document.querySelector('.main');
+const sideBar = document.querySelector('.sideBar');
+const projectDiv = document.querySelector('#projectTitles')
+const btnProject = document.querySelector('#projectBtn');
+const closeBtn = document.querySelector('#close')
+const todoForm = document.querySelector('.popupTask');
+const popupProjectDiv = document.querySelector('.popupProject');
+todoForm.addEventListener('click', (e) => {
+    e.preventDefault();
+})
+closeBtn.addEventListener('click', (e) => {
+    todoForm.style.display = 'none';
 
-const sideBar = document.querySelector('.sideBar')
-
-const btnProject = document.querySelector('#projectBtn')
+});
 
 
 function createProject(title) {
 
 
-    let projectDiv = document.createElement('div')
-    projectDiv.classList.add('project')
-    mainDiv.append(projectDiv)
+    // let projectDiv = document.createElement('div');
+    // projectDiv.classList.add('project');
+    // mainDiv.append(projectDiv);
 
-    let h2 = document.createElement('h2')
-    h2.textContent = title;
-    projectDiv.append(h2);
+    let h3 = document.createElement('h3');
+    h3.textContent = title;
+    projectDiv.append(h3);
+    h3.addEventListener('click', () => {
+        mainDiv.innerHTML = h3.textContent
+        mainDiv.append(buttonAdd)
+    })
 
-    let deleteBtn = document.createElement('button');
+    // let deleteBtn = document.createElement('button');
 
-    deleteBtn.textContent = "Delete"
-    projectDiv.append(deleteBtn)
+    // deleteBtn.textContent = "Delete";
+    // projectDiv.append(deleteBtn);
 
 
 
     let buttonAdd = document.createElement('button');
+    buttonAdd.textContent = "Add a Task";
+    buttonAdd.addEventListener('click', (e) => {
+        console.log('hi')
+        todoForm.style.display = 'flex';
+    })
 
-    buttonAdd.textContent = "Add a Task"
-    projectDiv.append(buttonAdd)
+
+    // projectDiv.append(buttonAdd);
 
 
 }
 
 
 
-
 function formMaker() {
+
+
+
+    // const projecForm = document.querySelector('#projectForm');
+    // projecForm.addEventListener('submit', (e) => {
+
+    //     e.preventDefault();
+
+
+    //     formAction();
+    //     popupProjectDiv.style.display = 'none'
+
+
+    //     // let title = document.querySelector('#projectTitle');
+    //     // // let value = title.value
+
+    //     // btnProject.disabled = false;
+    //     // projectArr.push(new Project(title.value));
+    //     // console.log(projectArr)
+    //     // console.log(title.value)
+
+
+
+    // })
+
+
 
     const form = document.createElement('form');
     form.setAttribute('action', 'submit');
@@ -49,6 +92,8 @@ function formMaker() {
         e.preventDefault();
         formAction(text);
         form.remove();
+        popupProjectDiv.style.display = 'none'
+
 
     });
 
@@ -61,25 +106,48 @@ function formMaker() {
     submit.setAttribute('type', 'submit');
     submit.setAttribute("value", "Submit");
 
-    sideBar.append(form);
+    popupProjectDiv.append(form);
     form.append(text);
     form.append(submit);
 
 }
 
 
-function formAction(input) {
-    let title = input.value
+function formAction(text) {
 
-    createProject(title)
+    // let title = document.querySelector('#projectTitle');
+    // let value = title.value
 
-    projectArr.push(new Project(title));
+    createProject(text.value)
+
     btnProject.disabled = false;
+    projectArr.push(new Project(text.value));
     console.log(projectArr)
-    console.log(title)
+    console.log(text.value)
 
 }
-export { formMaker, }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export { formMaker }
 
 
 
